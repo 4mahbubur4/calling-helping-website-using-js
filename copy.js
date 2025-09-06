631c3e31a2b1c7fad385cea793062bb6368b45e6
4mahbubur4 <mahbubur.mr4@gmail.com>
2025-09-06 23:39:48 +0600

    added copy and heart btn function and responsive mobile version

---
+++ copy.js
@@ -0,0 +1,23 @@
+
+document.querySelectorAll('.copy-btn').forEach(btn => {
+    btn.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+
+        const card = btn.closest('.card-container');
+
+
+        const textElement = card.querySelector('.card-text');
+        const textToCopy = textElement.innerText.trim();
+
+        alert("Number Copy : " + textToCopy)
+
+        const copyCnt = parseInt(document.getElementById('copy-cnt').innerText);
+        const copyCntTotal = copyCnt + 1;
+
+        document.getElementById('copy-cnt').innerText = copyCntTotal;
+
+   
+    });
+});
+
--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("successfully Connected")
+document.getElementById('all-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('pol-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('fire-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('amb-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('wom-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('anti-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('ele-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('brac-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
+document.getElementById('ban-rel-heart-btn')
+.addEventListener('click', function(e){
+    const heartCnt = parseInt(document.getElementById('heat-cnt').innerText);
+    const heartCntTotal = heartCnt+ 1;
+    
+    document.getElementById('heat-cnt').innerText = heartCntTotal;
+})
